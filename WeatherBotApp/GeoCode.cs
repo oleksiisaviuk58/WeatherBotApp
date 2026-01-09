e573ad6a6dbff500b374ed9cccfb33e9de12339c
Oleksii Saviuk <oleksiisaviuk58@gmail.com>
2026-01-09 22:57:21 +0200

    Has been added geocode service

--- WeatherBotApp/GeoCode.cs
+++ WeatherBotApp/GeoCode.cs
@@ -2,4 +2,15 @@ namespace WeatherTelegramBotApp;
 
 public class GeoCode
 {
+    [JsonPropertyName("name")]
+    public string City { get; set; }
+    
+    [JsonPropertyName("display_name")]
+    public string Country { get; set; }
+    
+    [JsonPropertyName("lat")]
+    public string Latitude { get; set; }
+    
+    [JsonPropertyName("lon")]
+    public string Longitude { get; set; }
 }
--- WeatherBotApp/GeoCodeService.cs
+++ WeatherBotApp/GeoCodeService.cs
@@ -2,4 +2,25 @@ namespace WeatherTelegramBotApp;
 
 public class GeoCodeService
 {
+    public static async Task<GeoCode> Get(string city)
+    {
+        string url = $"https://nominatim.openstreetmap.org/search?q={city}&format=json&limit=1&accept-language=uk";
+        var geo = await HttpClientService.Get<GeoCode[]>(url);
+
+        return geo[0];
+    }
+    
+    public static async Task<(string country, string city)> Get(string latitude, string longitude)
+    {
+        string url = $"https://nominatim.openstreetmap.org/reverse?lat={latitude}&lon={longitude}&format=json" +
+                     $"&accept-language=uk";
+        
+        var json = await HttpClientService.GetJson(url);
+        using var reader = JsonDocument.Parse(json);
+        
+        var city = reader.RootElement.GetProperty("address").GetProperty("city").GetString();
+        var country = reader.RootElement.GetProperty("address").GetProperty("country").GetString();
+        
+        return (country!, city!);
+    }
 }
