7d8bac2f1b3d1818fc9767b6fe51ff4d80c7a16a
Oleksii Saviuk <oleksiisaviuk58@gmail.com>
2026-01-09 20:20:58 +0200

    Has been added Namespaces class

--- WeatherBotApp/Program.cs
+++ WeatherBotApp/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using var tokenSource = new CancellationTokenSource();
+var token = tokenSource.Token;
+
+using StringReader telegramTokenReader = new StringReader("/Users/alexsavyuk/RiderProjects/" +
+                                                    "WeatherTelegramBotApp/WeatherBotApp/Token.txt");
+var telegramToken = await telegramTokenReader.ReadLineAsync();
+
+var bot = new TelegramBotClient(telegramToken!, cancellationToken: token);
+
+
+try
+{
+
+}
+catch
+{
+
+}
+finally
+{
+    
+}
