6a2f805cfed5d7b5a63fe997e665328b174b002f
Oleksii Saviuk <oleksiisaviuk58@gmail.com>
2026-01-09 20:42:45 +0200

    Has been added receiving methods

--- WeatherBotApp/Program.cs
+++ WeatherBotApp/Program.cs
@@ -1,22 +1,39 @@
-﻿using var tokenSource = new CancellationTokenSource();
-var token = tokenSource.Token;
+﻿using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+using var cts = new CancellationTokenSource();
 
 using StringReader telegramTokenReader = new StringReader("/Users/alexsavyuk/RiderProjects/" +
                                                     "WeatherTelegramBotApp/WeatherBotApp/Token.txt");
 var telegramToken = await telegramTokenReader.ReadLineAsync();
 
-var bot = new TelegramBotClient(telegramToken!, cancellationToken: token);
-
+var bot = new TelegramBotClient(telegramToken!, cancellationToken: cts.Token);
 
 try
 {
-
-}
-catch
-{
-
-}
-finally
-{
+    bot.OnError += OnError;
+    bot.OnMessage += OnMessage;
+    bot.OnUpdate += OnUpdate;
+    Console.WriteLine("Bot started!");
     
+    Console.ReadLine();
+    Console.WriteLine("Bot ended!");
+}
+catch (Exception ex) { Console.WriteLine(ex.Message); }
+finally { cts.Cancel(); }
+
+Task OnError(Exception exception, HandleErrorSource source)
+{
+    throw new NotImplementedException();
+}
+
+Task OnUpdate(Update update)
+{
+    throw new NotImplementedException();
+}
+
+Task OnMessage(Message message, UpdateType type)
+{
+    throw new NotImplementedException();
 }
